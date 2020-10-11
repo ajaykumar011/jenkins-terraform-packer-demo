88e2885ea6bf016e0cfd5eded9393077547b4bf1
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-11 08:00:16 +0530

    udpate

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 AWS_REGION="ap-south-1"
-cd jenkins-packer-demo2
+#cd jenkins-packer-demo
 S3_BUCKET="clodzone99"
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
