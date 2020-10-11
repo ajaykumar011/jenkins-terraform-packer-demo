59369809b82f4ebb86d280d9570cff62b2ca98cb
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-11 08:41:29 +0530

    udpate

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -6,4 +6,5 @@ S3_BUCKET="cloudzone99"
 aws s3 ls s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
-terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
+#terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
+terraform apply -auto-approve -var APP_INSTANCE_COUNT=1
