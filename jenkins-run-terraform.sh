65980528537aaa27041455c8381ba2e2cfc807d8
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-11 12:35:59 +0530

    udpate

--- jenkins-run-terraform.sh
+++ jenkins-run-terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-AWS_REGION="ap-south-1"
+AWS_REGION="us-east-1"
 #cd jenkins-packer-demo
 S3_BUCKET="cloudzone99"
 aws s3 ls s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
--- output.tf
+++ output.tf
@@ -2,6 +2,9 @@ output "app-ip" {
   value = [aws_instance.app-instance.*.public_ip]
 }
 
+output "app-ip" {
+  value = [aws_instance.app-instance.*.public_dns]
+}
 // output "s3-bucket" {
 //   value = aws_s3_bucket.terraform-state.bucket
 // }
