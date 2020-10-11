3dda776fff3a1a6b01577fb12697084d9d1eea80
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-11 08:23:05 +0530

    udpate

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -2,7 +2,8 @@
 set -ex
 AWS_REGION="ap-south-1"
 #cd jenkins-packer-demo
-S3_BUCKET="clodzone99"
+S3_BUCKET="cloudzone99"
+aws s3 ls s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
 terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
